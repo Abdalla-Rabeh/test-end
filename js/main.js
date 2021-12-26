8ab8202e8332165b1b168a59b7ff864576b7c6d7
Abdalla-Rabeh <abdallahcodeing@gmail.com>
2021-12-26 18:09:35 +0200

    test

--- js/main.js
+++ js/main.js
@@ -43,3 +43,26 @@ Fancybox.bind("[data-fancybox-plyr]", {
     },
   },
 });
+// carousel
+
+$(document).ready(function(){
+  $(".logo").fadeOut(5000);
+});
+
+let scroll_top =$('.Scroll-top')
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 100){
+        if(scroll_top.is(':hidden')){
+        $(scroll_top).fadeIn(400)
+        }
+    }else{
+        $(scroll_top).fadeOut(400)
+    }
+})
+$(".scroll_top").click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop:0
+        
+    },1000)
+})
